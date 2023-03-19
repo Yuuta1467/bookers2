f284e2b1c58ce6f66dae8f5ef28d9b75f3be3e00
Yuuta1467 <s1341011we@gmail.com>
2023-03-19 09:05:58 +0000

    [end16] 5-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,16 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  
+  def edit
+    @book =Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(params[:id])
+  end
 
   def destroy
     book = Book.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
    devise_for :users
    root to: "homes#top"
 
-   resources :books, only: [:new, :create, :index, :show, :destroy]
+   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
    resources :users, only: [:show, :edit, :index, :update]
 
    get "/homes/about" => "homes#about", as: "about"
