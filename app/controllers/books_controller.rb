0166cacc58866c732a0d85832fc98dfe3dd4457e
Yuuta1467 <s1341011we@gmail.com>
2023-03-20 07:46:27 +0000

    [end22] 5-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id)
     else
       render :new
-    end  
+    end
   end
 
   def index
@@ -17,12 +17,14 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.new
+    @user = @book.user
   end
-  
+
   def edit
     @book =Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
