ac95e8264c0b6a05ce3190721c01cf4478bcdfa6
Yuuta1467 <s1341011we@gmail.com>
2023-03-21 07:28:51 +0000

    [end31] 5-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,9 +28,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
+       redirect_to book_path(params[:id])
+    else
+      render:edit
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
- 
+
+  validates :name, presence: true
+
   has_one_attached :profile_image
 
   def get_profile_image(width, height)
